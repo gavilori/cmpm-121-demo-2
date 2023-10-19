f2a68df3f08db4571bc11cb7b1105b62c0ddfc1d
ofcourseitsgv <84610338+ofcourseitsgv@users.noreply.github.com>
2023-10-19 16:57:29 -0700

    Step 1 complete

--- src/main.ts
+++ src/main.ts
@@ -8,4 +8,16 @@ document.title = gameName;
 
 const header = document.createElement("h1");
 header.innerHTML = gameName;
+header.style.filter = "drop-shadow(6px 6px black)";
 app.append(header);
+
+const canvas = document.createElement("canvas");
+canvas.height = 256;
+canvas.width = 256;
+canvas.style.border = "solid 2pt black";
+canvas.style.backgroundColor = "white";
+canvas.style.borderRadius = "15px";
+canvas.style.filter = "drop-shadow(6px 6px black)";
+app.append(canvas);
+
+// const ctx = canvas.getContext("2d");
